ed0701678d67e830a2a03a037ad8930d7a912f34
Ashik D <ashikofficial455@gmail.com>
2025-07-22 11:08:17 +0530

    hero done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import { ArrowRight } from "lucide-react";
-import { easeIn, easeInOut, motion } from "motion/react";
+import { easeInOut, motion } from "motion/react";
 
 export default function Hero() {
   return (
-    <div className="container mx-auto flex-col flex items-center justify-center mt-32 w-max relative ">
+    <div className="container mx-auto flex-col flex items-center justify-center mt-34 w-max relative ">
       <Blobs />
       <Content />
     </div>
@@ -21,15 +21,19 @@ const Blobs = () => {
     <>
       <motion.div
         {...animateProps}
-        className="blob-1 bg-purple-300 size-80 rounded-full absolute mix-blend-multiply  left-[-2rem] blur-2xl opacity-50"
+        className="blob-1 bg-purple-300 size-80 rounded-full absolute mix-blend-multiply left-[-2rem]  opacity-50 blur-2xl"
       ></motion.div>
       <motion.div
         {...animateProps}
-        className="blob-1 bg-yellow-300 size-80 rounded-full absolute mix-blend-multiply   right-[-2rem] blur-2xl opacity-50"
+        className="blob-1 bg-purple-300 size-80 rounded-full absolute mix-blend-multiply right-[-1.5rem]  opacity-50 blur-2xl"
       ></motion.div>
       <motion.div
         {...animateProps}
-        className="blob-1 bg-pink-300 size-80  rounded-full mix-blend-multiply    absolute bottom-[-4rem] blur-2xl  opacity-50"
+        className="blob-1 bg-linear-to-b from-green-300 to-amber-300 size-80  rounded-full mix-blend-multiply  absolute top-[-2rem]   opacity-50 blur-2xl"
+      ></motion.div>
+      <motion.div
+        {...animateProps}
+        className="blob-1 bg-yellow-300/50 size-80  rounded-full mix-blend-multiply  absolute bottom-[-4rem]    blur-2xl"
       ></motion.div>
     </>
   );
@@ -38,14 +42,46 @@ const Blobs = () => {
 const Content = () => {
   return (
     <>
-      <div className="relative">
+      <div className="flex gap-[3rem] items-baseline">
+        <div className="space-y-[2rem] mb-2">
+          <motion.img
+            initial={{ x: 80, y: "50%", rotate: -20, opacity: 0 }}
+            animate={{ x: 0, y: "0", rotate: 0, opacity: 1 }}
+            src="/3.jpg"
+            alt=""
+            className="w-14 rounded drop-shadow-md drop-shadow-black/20 rotate-[-20deg]"
+          />
+          <motion.img
+            initial={{ x: 80, y: "50%", rotate: -20, opacity: 0 }}
+            animate={{ x: 0, y: "0", rotate: 0, opacity: 1 }}
+            src="/2.jpg"
+            alt=""
+            className="w-14 rounded drop-shadow-md drop-shadow-black/20 rotate-[-20deg]"
+          />
+        </div>
         <img src="/logo.svg" alt="Logo" className="drop-shadow-black size-40" />
-        <FloatingImages />
+        <div className="space-y-[2rem] mb-2">
+          <motion.img
+            initial={{ x: -80, y: "100%", rotate: -20, opacity: 0 }}
+            animate={{ x: 0, y: "0", rotate: 0, opacity: 1 }}
+            src="/4.jpg"
+            alt=""
+            className="w-14 rounded drop-shadow-md drop-shadow-black/20 rotate-[20deg]"
+          />
+          <motion.img
+            initial={{ x: -80, y: "100%", rotate: -20, opacity: 0 }}
+            animate={{ x: 0, y: "0", rotate: 0, opacity: 1 }}
+            src="/1.jpg"
+            alt=""
+            className="w-14 rounded drop-shadow-md drop-shadow-black/20 rotate-[20deg]"
+          />
+        </div>
       </div>
-      <h1 className="text-9xl condensed font-black tracking-tighter">
+
+      <h1 className="text-[10rem] leading-[10rem] font-black tracking-tighter">
         STYLE HUB
       </h1>
-      <p className="text-center text-balance max-w-[40rem] text-xs opacity-80 font-medium mt-1">
+      <p className="text-center text-balance max-w-[50rem] text-sm opacity-80 font-medium mt-1">
         A platform for discovering and sharing design styles, this space
         empowers creatives to explore unique aesthetics, curate personal
         collections, and connect with a community of like-minded designers.
@@ -74,32 +110,6 @@ const Content = () => {
           </span>
         </motion.button>
       </div>
-    </>
-  );
-};
-
-const FloatingImages = () => {
-  return (
-    <>
-      <motion.img src="1.jpg" alt="" className="w-10 absolute rounded " />
-      <motion.img
-        className="w-10 absolute rounded top-1/2 right-0"
-        initial={{ x: -50, rotate: 0 }}
-        animate={{ x: 70, rotate: 20 }}
-        // animate={{
-        //   x: 50,
-        //   y: 25,
-        //   rotate: 25,
-        // }}
-
-        src="2.jpg"
-        alt=""
-      />
-      <img
-        src="3.jpg"
-        alt=""
-        className="w-10 absolute rounded top-0 left-[-2rem] "
-      />
     </>
   );
 };
